f2e12a67354bee1d4cc5a10e2fc256980c13b475
kyoungho.moon <kyoungho.moon@purpleworks.co.kr>
2020-08-04 18:38:11 +0900

    4.2.2.1 assets 폴더 추가

---
+++ src/main/java/info/thecodinglive/config/WebConfig.java
@@ -0,0 +1,17 @@
+package info.thecodinglive.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/assets/", "/assets/");
+    }
+}
