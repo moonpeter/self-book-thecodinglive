b7010d75c6cc2d10957216f7e0aae3c1b637a432
kyoungho.moon <kyoungho.moon@purpleworks.co.kr>
2020-08-04 18:43:25 +0900

    4.2.2.2 리소스 주소에 MD5 값 설정

--- src/main/java/info/thecodinglive/config/WebConfig.java
+++ src/main/java/info/thecodinglive/config/WebConfig.java
@@ -3,6 +3,7 @@ package info.thecodinglive.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 
 @Configuration
@@ -10,8 +11,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-
         registry.addResourceHandler("/assets/**")
-                .addResourceLocations("classpath:/assets/", "/assets/");
+                .addResourceLocations("classpath:/assets/", "/assets/")
+        .setCachePeriod(60 * 60 * 24 * 365) //1년
+        .resourceChain(true)
+        .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
     }
 }
