28b19408a0a111a6d7572a81499a381ef4b60f70
kyoungho.moon <moonpeter@kakao.com>
2020-08-05 18:13:53 +0900

    5.3.3.1 PathVariable을 이용한 URL 표현

--- src/main/java/info/thecodinglive/controller/BasicController.java
+++ src/main/java/info/thecodinglive/controller/BasicController.java
@@ -3,11 +3,10 @@ package info.thecodinglive.controller;
 import info.thecodinglive.model.Todo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
@@ -28,5 +27,19 @@ public class BasicController {
     @RequestMapping(value = "/todor", method = RequestMethod.POST)
     public ResponseEntity<Todo> postBasicResponseEntity(@RequestParam (value = "todoTitle") String todoTitle){
         return new ResponseEntity(new Todo(counter.incrementAndGet(), todoTitle), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/todos/{todoId}", method = RequestMethod.GET)
+    public Todo getPath(@PathVariable int todoId){
+        Todo todo1 = new Todo(1L, "문서쓰기");
+        Todo todo2 = new Todo(2L, "기획회의");
+        Todo todo3 = new Todo(3L, "운동");
+
+        Map<Integer, Todo> todoMap = new HashMap<>();
+        todoMap.put(1, todo1);
+        todoMap.put(2, todo2);
+        todoMap.put(3, todo3);
+
+        return todoMap.get(todoId);
     }
 }
