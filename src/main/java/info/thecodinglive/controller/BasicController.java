6aa04a555cc3a2e40a761035c241a263e750a3ac
kyoungho.moon <moonpeter@kakao.com>
2020-08-05 17:59:51 +0900

    5.3.2.2 응답 헤더 활용

--- src/main/java/info/thecodinglive/controller/BasicController.java
+++ src/main/java/info/thecodinglive/controller/BasicController.java
@@ -1,6 +1,8 @@
 package info.thecodinglive.controller;
 
 import info.thecodinglive.model.Todo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,5 +23,10 @@ public class BasicController {
     @RequestMapping(value = "/todop", method = RequestMethod.POST)
     public Todo postBasic(@RequestParam(value = "todoTitle") String todoTitle){
         return new Todo(counter.incrementAndGet(), todoTitle);
+    }
+
+    @RequestMapping(value = "/todor", method = RequestMethod.POST)
+    public ResponseEntity<Todo> postBasicResponseEntity(@RequestParam (value = "todoTitle") String todoTitle){
+        return new ResponseEntity(new Todo(counter.incrementAndGet(), todoTitle), HttpStatus.CREATED);
     }
 }
