45773d5e57990d5397d0070d5585fa2444816d97
kyoungho.moon <moonpeter@kakao.com>
2020-08-05 16:58:45 +0900

    4.4.2.2 interceptor 등록

--- src/main/java/info/thecodinglive/config/WebConfig.java
+++ src/main/java/info/thecodinglive/config/WebConfig.java
@@ -1,23 +1,40 @@
 package info.thecodinglive.config;
 
+import info.thecodinglive.ExecuteTimeInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.GzipResourceResolver;
 import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public ExecuteTimeInterceptor executeTimeInterceptor() {
+        return new ExecuteTimeInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(executeTimeInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/assets/**")
                 .addResourceLocations("classpath:/assets/", "/assets/")
-        .setCachePeriod(60 * 60 * 24 * 365) //1년
-        .resourceChain(true)
-        .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new GzipResourceResolver())
+                .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"));
     }
+
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
