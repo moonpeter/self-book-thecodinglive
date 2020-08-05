ab91c9e9f0138bd281acda7944659819dedfafbc
kyoungho.moon <moonpeter@kakao.com>
2020-08-05 13:43:03 +0900

    4.3.2.1 변수 표현식

--- src/main/java/info/thecodinglive/controller/UIController.java
+++ src/main/java/info/thecodinglive/controller/UIController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class UIController {
     @RequestMapping(value = "/th")
@@ -11,5 +14,15 @@ public class UIController {
         model.addAttribute("message", "boot template");
 
         return "th";
+    }
+
+    @RequestMapping(value = "/th2")
+    public String templatePage2(Model model) {
+        Map<String, Object> pageMap = new HashMap<>();
+        pageMap.put("color", "red");
+        pageMap.put("name", "jam");
+        pageMap.put("price", 3000);
+        model.addAttribute("product", pageMap);
+        return "th2";
     }
 }
